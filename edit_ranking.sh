ad6b84767bee4a3d9a485378b8a794168a6f0894
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-05-27 18:37:28 -0300

    adicionando mudanca de artigos em destaque - ci

---
+++ edit_ranking.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#marllus - 27/05/21
+
+cat export.txt | cut -d',' -f1 | sort | uniq -c | sort -r | egrep '\ \/(tecnologia|poesia|sociedade|arte)' | head -4 | sed 's/^\ *//g' | cut -d' ' -f2 | sed 's/^\/.*\///g ; s/\.html//g' > ranking_posts
+
+#remove previous featured in posts
+find _posts/ -name "*.md" | xargs -n 1 sed -i '/^featured:\ true/d ; /^hidden:\ true/d'
+
+#add new param in featured posts
+#featured: true
+#hidden: true
+a=5
+for i in `cat ranking_posts`
+  do
+	  find _posts/ -name "*$i*" | xargs -n 1 sed -i '3 i featured: true\nhidden: true\nrating: '$a''
+  	  ((a--))
+  done
+
---
+++ export_goat.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+token=$1
+api=$2
+curl() {
+    \command curl \
+        -H 'Content-Type: application/json' \
+        -H "Authorization: Bearer $token" \
+        "$@"
+}
+
+# Start a new export, get ID from response object.
+id=$(curl -X POST "$api/export" | jq .id)
+
+# The export is started in the background, so we'll need to wait until it's finished.
+while :; do
+    sleep 1
+
+    finished=$(curl "$api/export/$id" | jq .finished_at)
+    if [ "$finished" != "null" ]; then
+        # Download the export.
+        curl "$api/export/$id/download" | gzip -d
+
+        break
+    fi
+done
+
