1488831c50cefbc8da86e10c8d9720504c05850b
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-05-27 19:13:54 -0300

    rating a - script featured

--- edit_ranking.sh
+++ edit_ranking.sh
@@ -4,11 +4,12 @@
 cat export.txt | cut -d',' -f1 | sort | uniq -c | sort -r | egrep '\ \/(tecnologia|poesia|sociedade|arte)' | head -4 | sed 's/^\ *//g' | cut -d' ' -f2 | sed 's/^\/.*\///g ; s/\.html//g' > ranking_posts
 
 #remove previous featured in posts
-find _posts/ -name "*.md" | xargs -n 1 sed -i '/^featured:\ true/d ; /^hidden:\ true/d'
+find _posts/ -name "*.md" | xargs -n 1 sed -i '/^featured:\ true/d ; /^hidden:\ true/d ; /^rating:\ [1-5]\{1\}/d'
 
 #add new param in featured posts
 #featured: true
 #hidden: true
+#rating: a
 a=5
 for i in `cat ranking_posts`
   do
