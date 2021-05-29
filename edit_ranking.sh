db2a47bc9e9cbaa482ea9779f77f1f353a20773c
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-05-29 09:49:14 -0300

    alterando script workflow github actions - telegram

--- edit_ranking.sh
+++ edit_ranking.sh
@@ -3,6 +3,7 @@
 
 cat export.txt | cut -d',' -f1 | sort | uniq -c | sort -r | egrep '\ \/(tecnologia|poesia|sociedade|arte)' | head -4 | sed 's/^\ *//g' | cut -d' ' -f2 | sed 's/^\/.*\///g ; s/\.html//g' > ranking_posts
 cat export.txt | cut -d',' -f1,2 | sort | uniq -c | sort -r | egrep '\ \/(tecnologia|poesia|sociedade|arte)' | head -4 | sed 's/^\ .//g' | cut -d',' -f2 > ranking_posts_name
+sed -i '1 i\Há uma mudança nos artigos em destaque. Segue a lista em ordem decrescente dos mais vistos:' ranking_posts_name
 
 #remove previous featured in posts
 find _posts/ -name "*.md" | xargs -n 1 sed -i '/^featured:\ true/d ; /^hidden:\ true/d ; /^rating:\ [1-5]\{1\}/d'
