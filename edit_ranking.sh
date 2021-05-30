7cdf2e50ce206e3a6bfa7e19f797a2bab0e4e0cb
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-05-30 13:25:57 -0300

    notificações configuradas - ajeitando view pelo telegram

--- edit_ranking.sh
+++ edit_ranking.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#marllus - 27/05/21
+#marllus.com
 
 cat export.txt | cut -d',' -f1 | sort | uniq -c | sort -r | egrep '\ \/(tecnologia|poesia|sociedade|arte)' | head -4 | sed 's/^\ *//g' | cut -d' ' -f2 | sed 's/^\/.*\///g ; s/\.html//g' > ranking_posts
-cat export.txt | cut -d',' -f1,2 | sort | uniq -c | sort -r | egrep '\ \/(tecnologia|poesia|sociedade|arte)' | head -4 | sed 's/^\ .//g' | cut -d',' -f2 | sed 's/^/-\ /g' > ranking_posts_name
-sed -i '1 i\Há uma mudança nos artigos em destaque. Segue a lista em ordem decrescente dos mais vistos:' ranking_posts_name
+cat export.txt | cut -d',' -f1,2 | sort | uniq -c | sort -r | egrep '\ \/(tecnologia|poesia|sociedade|arte)' | head -4 | sed 's/^\ .//g' | cut -d',' -f1,2 | sed 's/^\ *\([0-9]\+\)\ *.*html,\(.*$\)/\2 (\1 views)/g' > ranking_posts_name
+sed -i '1 i\Há uma mudança nos artigos em destaque. Segue a lista dos mais vistos:' ranking_posts_name
 
 #remove previous featured in posts
 find _posts/ -name "*.md" | xargs -n 1 sed -i '/^featured:\ true/d ; /^hidden:\ true/d ; /^rating:\ [1-5]\{1\}/d'
