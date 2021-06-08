be27bfaf60ee6488a37c2034dc6fc9db874b2708
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-06-08 17:17:38 -0300

    workflow agendamento artigos - ok

--- publica_agendados.sh
+++ publica_agendados.sh
@@ -7,11 +7,19 @@ echo $dateToday
 
 #move file to folder categories and rename with .md
 cd _posts/agendados/
+rm -rf logs_publicados
 for i in `ls -1`
   do
 	  dataArquivo=$(ls $i | sed 's/\([0-9]\{4\}\)-\([0-9]\{2\}\)-\([0-9]\{2\}\).*/\3\2\1/g')
 	  if [ "$dateToday" = "$dataArquivo" ]; then
 		  pasta=$(cat $i | grep categories | sed 's/^categories\:\ //g')
+		  FILE=logs_publicados
+        	  if [ ! -f "$FILE" ]; then
+			  echo "Artigos publicados hoje:" > logs_publicados
+			  cat $i | grep title | sed 's/^title\:\ /- /g' >> logs_publicados
+        	  else
+          		  cat $i | grep title | sed 's/^title\:\ /- /g' >> logs_publicados
+        	  fi
 		  echo "movendo $i para pasta $pasta"
 		  mv $i ../$pasta/$i.md
 	  fi
