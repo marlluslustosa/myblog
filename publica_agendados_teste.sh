8eb0c4c1148bfa61cbe13720eb74ee39a22f23d3
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-06-08 17:06:11 -0300

    workflow agendamento artigos

--- publica_agendados_teste.sh
+++ publica_agendados_teste.sh
@@ -13,14 +13,14 @@ for i in `ls -1`
 	  dataArquivo=$(ls $i | sed 's/\([0-9]\{4\}\)-\([0-9]\{2\}\)-\([0-9]\{2\}\).*/\3\2\1/g')
 	  if [ "$dateToday" = "$dataArquivo" ]; then
 		  pasta=$(cat $i | grep categories | sed 's/^categories\:\ //g')
-		  echo "movendo $i para pasta $pasta"
-		  mv $i ../$pasta/$i
 		  FILE=logs_publicados
         	  if [ ! -f "$FILE" ]; then
-			  echo "Artigos publicados hoje: \n" > logs_publicados
+			  echo "Artigos publicados hoje:" > logs_publicados
 			  cat $i | grep title | sed 's/^title\:\ /- /g' >> logs_publicados
         	  else
           		  cat $i | grep title | sed 's/^title\:\ /- /g' >> logs_publicados
-        	  fi		  
+        	  fi
+		  echo "movendo $i para pasta $pasta"
+		  mv $i ../$pasta/$i
 	  fi
   done
