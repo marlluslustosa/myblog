75006d532da8dfb5bec2aa0b2cec3da0d3fcd8b4
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2024-02-14 18:01:59 -0300

    alterando grep no edit_ranking - erro no workflow

--- edit_ranking.sh
+++ edit_ranking.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #marllus.com
 
-cat export.txt | cut -d',' -f1 | sort | uniq -c | sort -r -n | egrep '\ \/(tecnologia|ciencia|poesia|sociedade|arte)' | head -4 | sed 's/^\ *//g' | cut -d' ' -f2 | sed 's/^\/.*\///g ; s/\.html//g' > ranking_posts
-cat export.txt | cut -d',' -f1,2 | sort | uniq -c | sort -r -n | egrep '\ \/(tecnologia|ciencia|poesia|sociedade|arte)' | head -4 | sed 's/^\ .//g' | cut -d',' -f1,2 | sed 's/^\ *\([0-9]\+\)\ *.*html,\(.*$\)/- \2 (\1 views)/g' > ranking_posts_name
+cat export.txt | cut -d',' -f1 | sort | uniq -c | sort -r -n | grep -E '/(tecnologia|ciencia|poesia|sociedade|arte)' | head -4 | sed 's/^\ *//g' | cut -d' ' -f2 | sed 's/^\/.*\///g ; s/\.html//g' > ranking_posts
+cat export.txt | cut -d',' -f1,2 | sort | uniq -c | sort -r -n | grep -E '/(tecnologia|ciencia|poesia|sociedade|arte)' | head -4 | sed 's/^\ .//g' | cut -d',' -f1,2 | sed 's/^\ *\([0-9]\+\)\ *.*html,\(.*$\)/- \2 (\1 views)/g' > ranking_posts_name
 sed -i '1 i\Há uma mudança nos artigos em destaque. Segue a lista dos mais vistos:' ranking_posts_name
 
 #remove previous featured in posts
